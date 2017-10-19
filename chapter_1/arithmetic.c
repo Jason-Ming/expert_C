19873f61388daf781f02e622f78c2ce8442b99f2
jason <jason@github.com>
2017-10-19 02:47:46 -0700

    add a example about type conversion

--- chapter_1/arithmetic.c
+++ chapter_1/arithmetic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+int array[] = {23, 34, 12, 17, 204, 99, 16};
+int array0[] = {};
+int array1[] = {23};
+int array2[] = {23, 34};
+#define TOTAL_ELEMENTS(array) (sizeof(array)/sizeof(array[0]))
+
 void main()
 {
 	if( -1 < (unsigned char)1)
@@ -10,4 +16,16 @@ void main()
 	{
 		printf("-1 is NOT less than (unsigned char)1: K&R semantics.\n");
 	}
+	
+	int d = -1, x = 10;
+	
+	unsigned int s = TOTAL_ELEMENTS(array);
+	printf("s = %u.\n", s);
+	
+	if(d < s - 2)
+	{
+		x = array[d+2];
+	}
+	
+	printf("x = %d.\n", x);
 }
