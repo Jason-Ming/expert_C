05294f6405691973ee8795a8eca25879e81ee042
jason <jason@github.com>
2017-10-19 23:44:42 -0700

    add new file

---
+++ chapter_2/string_connect.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("\
+	A favorite children's book,\
+	is 'muffy Gets It: the hilarious tale of a cat,\
+	a boy, and his machine gun'");
+	
+	printf("A second favorite children's book is "
+	"'Thoms the tank engine and Naughty Enginedriver who"
+	" tied down Thoms's boiler safety value'");
+}
